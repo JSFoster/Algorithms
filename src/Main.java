a980dbf5d8ed7f67baf5366754377e9474368438
Joel <jfoste33@myune.edu.au>
2019-06-26 17:37:26 +1000

    Selection Sort + Insertion Sort

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        int[] unsorted = {4, 12, 2, 24, 7, 1, 20};
+        int[] array = {4, 12, 2, 24, 7, 1, 20, 1};
 
-        //selectionSort(unsorted);
-        insertionSort(unsorted);
+        System.out.println("Selection Sort results:");
+        selectionSort(array);
+
+        System.out.println("Insertion Sort results:");
+        insertionSort(array);
     }
 
-    public static void selectionSort(int[] unsorted) {
+    public static void print(int[] array) {
+        // Print sorted array
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println();
+    }
+
+    public static void selectionSort(int[] array) {
         int pnt = 0;
         int lowest = 0;
 
         do {
-            for (int i = pnt; i < unsorted.length; i++) {
-                if (unsorted[i] < unsorted[lowest]) {
+            for (int i = pnt; i < array.length; i++) {
+                if (array[i] < array[lowest]) {
                     lowest = i;
                 }
             }
-            int tmp = unsorted[pnt];
-            unsorted[pnt] = unsorted[lowest];
-            unsorted[lowest] = tmp;
+            int tmp = array[pnt];
+            array[pnt] = array[lowest];
+            array[lowest] = tmp;
             pnt++;
-            if (pnt < unsorted.length) {
+            if (pnt < array.length) {
                 lowest = pnt;
             }
-        } while (pnt < unsorted.length);
+        } while (pnt < array.length);
 
-        // Print sorted array
-        for (int i = 0; i < unsorted.length; i++) {
-            System.out.println(unsorted[i]);
-        }
+        print(array);
     }
 
-    public static void insertionSort(int[] unsorted) {
-        for (int i = 0; i < unsorted.length; i++) {
-            int key = unsorted[i];
-            
+    public static void insertionSort(int[] array) {
+        int key = 0;
+
+        for (int i = key + 1; i < array.length; i++) {
+            int j = key + 1;
+            if (array[j] < array[key]) {
+                while (j-1 >= 0 && array[j] < array[j-1]) {
+                    int tmp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = tmp;
+                    j--;
+                }
+            }
+            key++;
         }
 
-        // Print sorted array
-        for (int i = 0; i < unsorted.length; i++) {
-            System.out.println(unsorted[i]);
-        }
+        print(array);
     }
 }
